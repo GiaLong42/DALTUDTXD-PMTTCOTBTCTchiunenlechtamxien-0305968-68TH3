46968ed24eab472b37267928697f533abfb6655a
khanhdg_0305968 <97107489+GiaLong42@users.noreply.github.com>
2026-03-18 09:55:42 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DALTUDTXD_AppTTvaTKCOTBTCTCHIUNENLECHTAMXIEN_DangGiaKhanh_0305968_68TH3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using Fluent; 
+
+namespace DALTUDTXD_AppTTvaTKCOBTCTCHIUNENLECHTAMXIEN_DangGiaKhanh_0305968_68TH3
+{
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnXuatEtabs_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Đang kết nối với ETABS qua API...\nVui lòng đảm bảo ETABS đang mở mô hình của bạn.",
+                            "Kết nối ETABS", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            
+        }
+    }
+}
